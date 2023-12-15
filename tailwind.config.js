6a87eff66b48751e4ea0a54fdbb7af21d321b19b
AREEBA-MUHAMMAD-AIJAZ <96327834+AREEBA-MUHAMMAD-AIJAZ@users.noreply.github.com>
2023-12-15 18:16:11 +0500

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        "heroSection-image": "url('/vector8.png')"
+        "heroSection-image": "url('/assets/vector8.png')"
       }
     },
   },
