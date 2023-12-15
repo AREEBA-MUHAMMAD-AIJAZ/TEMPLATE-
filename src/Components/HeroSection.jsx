1034e9c0f7d31809e30f82fae044f9f040dc8bff
AREEBA-MUHAMMAD-AIJAZ <96327834+AREEBA-MUHAMMAD-AIJAZ@users.noreply.github.com>
2023-12-15 18:10:04 +0500

    completed

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -39,7 +39,7 @@ import React from "react";
 
 function Herosection() {
   return (
-      <section className="text-white bg-[url(../../public/vector8.png)] bg-no-repeat bg-cover pt-20 ">
+      <section className="text-white bg-heroSection-image bg-no-repeat bg-cover pt-20 ">
         <div class="grid max-w-screen-xl md:px-16 px-8 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-36 ">
           <div class="mr-auto place-self-center lg:col-span-7">
             <h2 className=" relative font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
